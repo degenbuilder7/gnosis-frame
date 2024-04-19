8be07dcc53b3308482a74a106b862cd2e0368773
kamalishere <kamaleinstein1@gmail.com>
2024-04-19 04:02:51 +0000

    txn frame and components added

---
+++ src/components/Nav.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Nav.module.css";
+import { LogInWithAnonAadhaar, useAnonAadhaar } from "anon-aadhaar-react";
+import { useEffect } from "react";
+
+import { ConnectKitButton } from "connectkit";
+
+export function Navbar() {
+  const [anonAadhaar] = useAnonAadhaar();
+  useEffect(() => {
+    console.log("Anon Aadhaar status: ", anonAadhaar.status);
+  }, [anonAadhaar]);
+  
+  return (
+    <div className={styles.navContainer}>
+      <nav className={styles.nav}>
+        <div className={styles.navLeft}>
+          <Link href="/" className={`${styles.homeLink} ${styles.navLeft}`}>
+            GiftonFarcastergnosis
+          </Link>
+
+          </div>
+
+          <div className={styles.navMiddle}>
+              <Link href="/claim" className={`${styles.homeLink} ${styles.navMiddle}`}>
+                Claim
+              </Link>
+          </div>
+
+        <div className={styles.navRight}>
+          <div className={styles.navConnect}>
+          <ConnectKitButton.Custom>
+      {({ isConnected, isConnecting, show, hide, address, ensName, chain }) => {
+        return (
+          <button onClick={show}>
+            {isConnected ? address : "Connect Wallet"}
+          </button>
+        );
+      }}
+    </ConnectKitButton.Custom>
+          
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
